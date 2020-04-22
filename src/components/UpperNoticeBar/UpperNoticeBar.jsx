ccbfda8b957bb43645ca22688e0f910b4ef1b21d
Raghav Dhingra <raghav.dhingra15@gmail.com>
2020-04-23 00:57:50 +0530

    Add files via upload

--- src/components/UpperNoticeBar/UpperNoticeBar.jsx
+++ src/components/UpperNoticeBar/UpperNoticeBar.jsx
@@ -1,21 +1,13 @@
 import React, { Component } from "react";
 import { API_Domain } from "../../pages/baseDomain";
-import searchBar from "./assets/images/search.svg";
 import "./assets/css/uppernoticebar.css";
 import axios from "axios";
 
 class UpperNoticeBar extends Component {
   state = {
-    searchVisibility: false,
     notice: "",
+    componentVisibility: true,
   };
-
-  ToggleSearchBar = () => {
-    this.state.searchVisibility
-      ? this.setState({ searchVisibility: false })
-      : this.setState({ searchVisibility: true });
-  };
-
   componentDidMount = () => {
     axios({
       url: `${API_Domain.route}/api/v1/upper-notice`,
@@ -30,74 +22,75 @@ class UpperNoticeBar extends Component {
       ) {
         return null;
       } else {
-        console.log(data.data.current_notice[0].notice);
+        // console.log(data.data.current_notice[0].notice);
         this.setState({ notice: data.data.current_notice[0].notice });
       }
     });
 
-    axios({
-      url: `${API_Domain.route}/api/v1/event-list`,
-      method: "GET",
-      headers: {
-        Authorization: "Token 5292645558db342649f9c41b50edd7db560962d2",
-      },
-      // data: {
-      //   event_name: "HELLO",
-      //   event_link: "THIS",
-      //   event_date: "IS",
-      //   event_time: "AN",
-      //   event_venue: "EVENT",
-      // },
-    }).then((data) => console.log(data.data));
+    // axios({
+    //   url: `${API_Domain.route}/api/v1/event-list`,
+    //   method: "GET",
+    //   headers: {
+    //     Authorization: "Token 5292645558db342649f9c41b50edd7db560962d2",
+    //   },
+    // data: {
+    //   event_name: "HELLO",
+    //   event_link: "THIS",
+    //   event_date: "IS",
+    //   event_time: "AN",
+    //   event_venue: "EVENT",
+    // },
+    // }).then((data) => console.log(data.data));
   };
 
   render() {
+    // if (this.state.componentVisibility) {
     return (
       <>
-        <div className="UpperBarContainer">
-          <div className="container-big">
-            <div className="UpperContainerGrid">
-              <div className="Running-Line">
-                {this.state.notice === "" ? (
-                  ""
-                ) : (
+        {this.state.componentVisibility ? (
+          <div className="UpperBarContainer">
+            <div className="container-big">
+              <div className="UpperContainerGrid">
+                <div className="Running-Line">
                   <>
-                    {/* eslint-disable-next-line */}
-                    <marquee className="display-after-992">
-                      <b>Notice</b> - {this.state.notice}
-                    </marquee>
+                    <div>
+                      <div className="upper-notice-announcements">
+                        <div className="upper-notice-announcements-text">
+                          Announcements
+                        </div>
+                        <div className="upper-notice-announcements-notice">
+                          {this.state.notice === "" ? (
+                            <>
+                              <div style={{ display: "flex" }}>
+                                {" "}
+                                <svg className="loader-svg-upper">
+                                  <circle r="5px" className="upper-loader" />
+                                </svg>{" "}
+                                Loading...
+                              </div>
+                            </>
+                          ) : (
+                            <div className="upper-notice-announcements-notice-inner">
+                              {this.state.notice}
+                            </div>
+                          )}
+                        </div>
+                      </div>
+                    </div>
                   </>
-                )}
-              </div>
-              <div className="RightContainer-UpperBar">
-                <div
-                  className={`search-box-container ${
-                    this.state.searchVisibility
-                      ? ""
-                      : "RightContainer-Upper-Hide"
-                  }`}
-                >
-                  <input type="text" placeholder="Search..." />
                 </div>
                 <div
-                  className={`UpperBar-LoginBtn UpperBar-SearchImg ${
-                    this.state.searchVisibility
-                      ? "UpperBar-LoginBtn-active"
-                      : ""
-                  }`}
-                  onClick={this.ToggleSearchBar}
+                  className="RightContainer-UpperBar"
+                  onClick={() => this.setState({ componentVisibility: false })}
                 >
-                  <img
-                    className="SearchImage"
-                    src={searchBar}
-                    alt="Search Bar"
-                  />
+                  &times;
                 </div>
-                <div className="UpperBar-LoginBtn">Login</div>
               </div>
             </div>
           </div>
-        </div>
+        ) : (
+          ""
+        )}
       </>
     );
   }
