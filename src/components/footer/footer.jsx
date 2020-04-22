4fe4a8b445c078362082cc2f9d28fa8013a36c25
Raghav Dhingra <raghav.dhingra15@gmail.com>
2020-04-23 00:57:18 +0530

    Add files via upload

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import "./assets/css/footer.css";
-import GTBIT_LOGO from "../TitleBar/assets/images/GTBIT-LOGO.png";
+import GTBIT_LOGO from "../../assets/images/gtbitlogo-old.png";
+// import GTBIT_LOGO from "../TitleBar/assets/images/GTBIT-LOGO.png";
 import { Link } from "react-router-dom";
 
 class Footer extends Component {
@@ -17,7 +18,10 @@ class Footer extends Component {
                   <div className="justify-center">
                     <img src={GTBIT_LOGO} width="100px" alt="GTBIT Logo" />
                   </div>
-                  <div className="justify-center">
+                  <div
+                    className="justify-center"
+                    style={{ color: "var(--primaryColor)" }}
+                  >
                     Guru Tegh Bahadur Institute Of Technology
                   </div>
                 </div>
@@ -30,9 +34,7 @@ class Footer extends Component {
               </div>
               <div className="footer-index-2">
                 <div className="footer-links-container">
-                  <div style={{ fontWeight: "bold", fontSize: "1.2em" }}>
-                    Admissions
-                  </div>
+                  <div className="footer-links-headers">Admissions</div>
                   <ul style={{ margin: "0 0.5em" }}>
                     <li>
                       <Link to="#" className="footer-index-links">
@@ -57,9 +59,7 @@ class Footer extends Component {
                   </ul>
                 </div>
                 <div className="footer-links-container">
-                  <div style={{ fontWeight: "bold", fontSize: "1.2em" }}>
-                    Other Links
-                  </div>
+                  <div className="footer-links-headers">Other Links</div>
                   <ul style={{ margin: "0 0.5em" }}>
                     <li>
                       <Link to="#" className="footer-index-links">
@@ -84,11 +84,50 @@ class Footer extends Component {
                   Any Queries? Ask here
                 </div>
                 <div className="footer-ask-here-container">
-                  <div> <input className="footer-ask-query-input" placeholder="Name" type="text" /> </div>
-                  <div> <input className="footer-ask-query-input" placeholder="Email Address" type="email" /> </div>
-                  <div> <input className="footer-ask-query-input" placeholder="Phone Number" type="number" /> </div>
-                  <div> <textarea className="footer-ask-query-input" placeholder="Any Query"></textarea> </div>
-                  <div style={{display:"flex",flexDirection:"row",justifyContent:"flex-end"}}> <button className="footer-ask-query-btn"> Ask the query </button> </div>
+                  <div>
+                    {" "}
+                    <input
+                      className="footer-ask-query-input"
+                      placeholder="Name"
+                      type="text"
+                    />{" "}
+                  </div>
+                  <div>
+                    {" "}
+                    <input
+                      className="footer-ask-query-input"
+                      placeholder="Email Address"
+                      type="email"
+                    />{" "}
+                  </div>
+                  <div>
+                    {" "}
+                    <input
+                      className="footer-ask-query-input"
+                      placeholder="Phone Number"
+                      type="number"
+                    />{" "}
+                  </div>
+                  <div>
+                    {" "}
+                    <textarea
+                      className="footer-ask-query-input"
+                      placeholder="Any Query"
+                    ></textarea>{" "}
+                  </div>
+                  <div
+                    style={{
+                      display: "flex",
+                      flexDirection: "row",
+                      justifyContent: "flex-end",
+                    }}
+                  >
+                    {" "}
+                    <button className="footer-ask-query-btn">
+                      {" "}
+                      Ask the query{" "}
+                    </button>{" "}
+                  </div>
                 </div>
               </div>
             </div>
@@ -102,7 +141,14 @@ class Footer extends Component {
                 </Link>
               </div>
               <div className="footer-webteam">
-                Developed by <Link className="footer-index-links" to="#" style={{ fontWeight: "bold" }}>Student Web Team, GTBIT</Link>
+                Developed by{" "}
+                <Link
+                  className="footer-index-links"
+                  to="#"
+                  style={{ fontWeight: "bold" }}
+                >
+                  Student Web Team, GTBIT
+                </Link>
               </div>
             </div>
           </div>
