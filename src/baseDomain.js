2fe3e498b2aa62a42b72fbe12a292f809b8a7d9c
Raghav Dhingra <raghav.dhingra15@gmail.com>
2020-04-28 02:31:29 +0530

    Add files via upload

---
+++ src/baseDomain.js
@@ -0,0 +1,5 @@
+const API_Domain = {
+  route: "http://127.0.0.1:8000",
+  // route: "https://gtbit-backend.herokuapp.com",
+};
+export { API_Domain };
--- src/router.jsx
+++ src/router.jsx
@@ -1,6 +1,10 @@
 import React, { Component } from "react";
 import "./assets/css/default.css";
+import UpperNoticeBar from "./components/UpperNoticeBar/UpperNoticeBar";
+import NavigationBar from "./components/NavigationBar/NavigationBar";
 import Home from "./pages/HomePage/home";
+import Error404Page from "./pages/Error404Page/Error404Page";
+import Footer from "./components/footer/footer";
 import { Switch, Route } from "react-router-dom";
 
 class Routes extends Component {
@@ -8,7 +12,13 @@ class Routes extends Component {
   render() {
     return (
       <Switch>
-        <Route exact path="/" component={Home} />
+        <Route path="/">
+          <UpperNoticeBar />
+          <NavigationBar />
+          <Route exact path="/" component={Home} />
+          <Route exact path="/error-404" component={Error404Page} />
+          <Footer />
+        </Route>
       </Switch>
     );
   }
