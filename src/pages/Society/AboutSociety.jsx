7f16c89f30c4b54ac08ca1ed2a4bd5abeac31e7a
Sachin negi <san.negi.77777@gmail.com>
2020-04-28 19:16:20 +0530

    heading css

--- src/pages/Society/AboutSociety.jsx
+++ src/pages/Society/AboutSociety.jsx
@@ -3,19 +3,18 @@ import { Carousel } from "antd";
 import "./assets/society.css";
 import SocietyEvents from "./SocietyEvents";
 import iipcImg from "./assets/image/iipc.png";
+import grpImage from "./assets/image/group.jpeg";
 
 export class AboutSociety extends Component {
   render() {
     return (
       <div className="container-fluid">
-        <div className="Sheading">
-          <h1 className="text-center text-dark m-0">IIPC</h1>
-          <p className="text-secondary text-center">
-            Industrial Institute Partnership Cell
-          </p>
+        <div className="Sheading border w-50 mt-3">
+          <h1 className="m-0">IIPC</h1>
+          <p>Industrial Institute Partnership Cell</p>
         </div>
 
-        <div className="row">
+        <div className="row mt-4">
           <div className="col">
             <div className="row justify-content-end">
               <div className="col">
@@ -35,7 +34,7 @@ export class AboutSociety extends Component {
           </div>
         </div>
 
-        <div className="row">
+        <div className="row my-2">
           <div className="col">
             <div className="row justify-content-end">
               <div className="col">
@@ -59,25 +58,23 @@ export class AboutSociety extends Component {
           <div className="col-10 col-sm-8">
             <Carousel autoplay>
               <div>
-                <h3>1</h3>
+                <img src={grpImage} />
               </div>
               <div>
-                <h3>2</h3>
+                <img src={grpImage} />
               </div>
               <div>
-                <h3>3</h3>
-              </div>
-              <div>
-                <h3>4</h3>
+                <img src={grpImage} />
               </div>
             </Carousel>
           </div>
         </div>
 
-        <div className="societydesc" 
-        //style={{backgroundImage:`url(${grpImage})`}}
+        <div
+          className="societydesc"
+          //style={{backgroundImage:`url(${grpImage})`}}
         >
-        <img src={iipcImg} className="aboutBgImg"/>
+          <img src={iipcImg} className="aboutBgImg" />
           <div className="text-secondary">About IIPC</div>
           <div>
             Industry Institute Partnership Cell(IIPC) enhances the interface
@@ -94,7 +91,7 @@ export class AboutSociety extends Component {
             vel dicta.
           </div>
         </div>
-        <SocietyEvents/>
+        <SocietyEvents />
       </div>
     );
   }
