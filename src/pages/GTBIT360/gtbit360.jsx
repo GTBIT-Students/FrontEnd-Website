15b0a32528824087e35deb22928c59b2fe127c73
Raghav Dhingra <raghav.dhingra15@gmail.com>
2020-05-26 02:04:44 +0530

    Add files via upload

---
+++ src/pages/GTBIT360/gtbit360.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+
+class GTBIT360 extends Component {
+  state = {};
+  render() {
+    return (
+      <>
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!4v1589223574296!6m8!1m7!1sCAoSLEFGMVFpcE1Ybm9Hd092NHM1YzlzU1JNVFlLN2ZxYnBhUXNIMUJmTldocm1H!2m2!1d28.6317868!2d77.1164821!3f40!4f20!5f0.7820865974627469"
+          width="100%"
+          title="GTBIT 360 View"
+          height="450"
+          frameBorder="0"
+          style={{ border: 0 }}
+          allowFullScreen=""
+          aria-hidden="false"
+          tabIndex="0"
+        ></iframe>
+      </>
+    );
+  }
+}
+
+export default GTBIT360;
