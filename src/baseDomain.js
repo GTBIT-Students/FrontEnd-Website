df28cb6feea11175a33bacf6b35d384be8b74b05
Raghav Dhingra <raghav.dhingra15@gmail.com>
2020-05-26 02:02:23 +0530

    Add files via upload

--- src/baseDomain.js
+++ src/baseDomain.js
@@ -1,5 +1,6 @@
 const API_Domain = {
-  route: "http://127.0.0.1:8000",
-  // route: "https://gtbit-backend.herokuapp.com",
+  route: "http://127.0.0.1:8000/api/v1",
+  // route: "https://gtbit-backend.herokuapp.com/api/v1",
+  API_Token: "5292645558db342649f9c41b50edd7db560962d2",
 };
-export { API_Domain };
+export default API_Domain;
