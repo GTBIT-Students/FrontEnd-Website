ce2ec8596f6c0459d31b2a14f35226e2addeb0be
Raghav Dhingra <raghav.dhingra15@gmail.com>
2020-05-26 02:03:10 +0530

    Add files via upload

---
+++ src/SideNavBar.js
@@ -0,0 +1,63 @@
+const SideNavObj = {
+  Society: {
+    heading: "Explore Socities",
+    elements: [
+      {
+        name: "Technical Societies",
+        link: "/society/#technical",
+      },
+      {
+        name: "Religious Societies",
+        link: "/society/#religious",
+      },
+      {
+        name: "Cultural Socities",
+        link: "/society/#cultural",
+      },
+      {
+        name: "Miscellaneous Societies",
+        link: "/society/#miscellaneous",
+      },
+    ],
+  },
+  SideEventNotice: {
+    heading: "Discover More",
+    elements: [
+      {
+        name: "Latest Notices",
+        link: "/notice",
+      },
+      {
+        name: "Upcoming Events",
+        link: "/event",
+      },
+      {
+        name: "Achievements",
+        link: "/achievement",
+      },
+    ],
+  },
+  Deparments: {
+    heading: "Explore Departments",
+    elements: [
+      {
+        name: "Computer Science",
+        link: "/department/computer-science",
+      },
+      {
+        name: "Information Technology",
+        link: "/department/information-technology",
+      },
+      {
+        name: "Electronics and Communication",
+        link: "/department/electronics-and-communication",
+      },
+      {
+        name: "Electrical and Electronics",
+        link: "/department/electrical-and-electronics",
+      },
+    ],
+  },
+};
+
+export default SideNavObj;
