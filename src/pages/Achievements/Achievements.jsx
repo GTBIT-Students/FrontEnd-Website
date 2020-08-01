dda3472951b3bd463fae615cae9a5e86767feb9a
Raghav Dhingra <raghav.dhingra15@gmail.com>
2020-08-01 16:30:50 +0530

    Add files via upload

--- src/pages/Achievements/Achievements.jsx
+++ src/pages/Achievements/Achievements.jsx
@@ -1,18 +1,44 @@
 import React, { Component } from "react";
+import axios from "axios";
+import API_Domain from "../../baseDomain";
+import Loader from "../../components/Loader/loader";
 import SideNavPane from "../../components/SideNavPane/SideNavPane";
 import SideNavObj from "../../SideNavBar";
-import "./assets/css/achievements.css";
-import Achievement from "./assets/images/achievement-1.jpg";
+// import Achievement from "./assets/images/achievement-1.jpg";
 import PhotoViewer from "./assets/component/PhotoViewer";
+import "./assets/css/achievements.css";
 
 class Achievements extends Component {
   state = {
-    image: "",
+    achievement: [],
+    image: null,
+    details: "",
     isViewerOpened: false,
+    isDetails: false,
   };
 
-  toggleViewer = (img) => {
-    this.setState({ isViewerOpened: !this.state.isViewerOpened, img: img });
+  toggleViewer = (img, details) => {
+    this.setState({
+      isViewerOpened: !this.state.isViewerOpened,
+      image: img,
+      isDetails: false,
+      details: details,
+    });
+  };
+  handleText = () => {
+    this.setState({ isDetails: !this.state.isDetails });
+  };
+
+  componentDidMount = () => {
+    axios({
+      url: `${API_Domain.route}/achievements`,
+      method: "GET",
+      headers: {
+        Authorization: `Token ${API_Domain.API_Token}`,
+      },
+    }).then((data) => {
+      this.setState({ achievement: data.data.achievement });
+    });
   };
 
   render() {
@@ -20,7 +46,8 @@ class Achievements extends Component {
       <>
         {this.state.isViewerOpened ? (
           <PhotoViewer
-            image={this.state.image}
+            image={this.state}
+            handleText={this.handleText}
             toggleViewer={this.toggleViewer}
           />
         ) : null}
@@ -29,26 +56,40 @@ class Achievements extends Component {
             <div>
               <h2 className="director-heading">Achievements</h2>
               <div className="director-page-main-content">
-                <div className="achievements-container-grid">
-                  {/* Achievements Content */}
-                  <div
-                    className="achievements-content-container"
-                    onClick={() => this.toggleViewer(Achievement)}
-                  >
-                    <div className="achievements-image-content-grid">
+                {this.state.achievement.length ? (
+                  <div className="achievements-container-grid">
+                    {/* Achievements Content */}
+                    {this.state.achievement.map((achievement, index) => (
                       <div
-                        className="achievement-image"
-                        style={{ backgroundImage: `url(${Achievement})` }}
-                      ></div>
-                      <div className="achievement-text three-dots">
-                        <span className="bold">Title</span>
-                        <br />
-                        <span>May 30, 2020</span>
+                        key={`Achievement-${index}`}
+                        className="achievements-content-container"
+                        onClick={() =>
+                          this.toggleViewer(
+                            achievement.image,
+                            achievement.description
+                          )
+                        }
+                      >
+                        <div className="achievements-image-content-grid">
+                          <div
+                            className="achievement-image"
+                            style={{
+                              backgroundImage: `url(${achievement.image})`,
+                            }}
+                          ></div>
+                          <div className="achievement-text three-dots">
+                            <span className="bold">{achievement.title}</span>
+                            <br />
+                            <span>{achievement.date}</span>
+                          </div>
+                        </div>
                       </div>
-                    </div>
+                    ))}
+                    {/* Achievements Content */}
                   </div>
-                  {/* Achievements Content */}
-                </div>
+                ) : (
+                  <Loader />
+                )}
               </div>
             </div>
             <div>
